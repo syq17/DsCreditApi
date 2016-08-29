85a2c9cb5069cab49ad17f7a73c3f0a6b9aa8ffe
syq <123123>
2016-08-29 08:55:30 +0800

    初始化apidemo

---
+++ apidemo/src/main/java/credit/Client.java
@@ -0,0 +1,60 @@
+package credit;
+
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by syq on 2016/8/28.
+ */
+public class Client {
+
+    private static final String APIKEY = "161c5ce6-7385-48db-bf68-bc3a8e83";
+
+    private static final String SECRETKEY = "536cee574c5d05df15b9df7b8236d53d4db96841a504f13ca462eb761152de2d";
+
+    private static final String APIHOST = "http://127.0.0.1:8082/credit/api/v1/query";
+
+    /**
+     * 普通接口请求示例
+     *
+     * @throws Exception
+     */
+    private static void apiRequest() throws Exception {
+        final String channelNo = "CH1709907004";
+        final String interfaceName = "mailboxAttribution";
+        long timestamp = new Date().getTime();
+        Header header = new Header(APIKEY, channelNo, interfaceName, timestamp);
+
+        CreditRequest creditRequest = new CreditRequest(APIHOST, header);
+
+        Map<String, Object> payload = new HashMap<>();
+        payload.put("email", "148371250@qq.com");
+
+        creditRequest.setPayload(payload);
+
+        creditRequest.signByKey(SECRETKEY);
+
+        String result = creditRequest.request();
+
+        System.out.println(result);
+    }
+
+
+    /**
+     * linkface 带文件上传的接口请求示例
+     *
+     * @throws Exception
+     */
+    private static void apiLinkfaceRequest() throws Exception {
+
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        apiRequest();
+    }
+
+
+}
---
+++ apidemo/src/main/java/credit/Header.java
@@ -0,0 +1,40 @@
+package credit;
+
+/**
+ * Created by syq on 2016/8/28.
+ */
+public class Header {
+
+    private String apiKey;
+
+    private String channelNo;
+
+    private String interfaceName;
+
+    private long timestamp;
+
+
+    public Header(String apiKey, String channelNo, String interfaceName, long timestamp) {
+        this.apiKey = apiKey;
+        this.channelNo = channelNo;
+        this.interfaceName = interfaceName;
+        this.timestamp = timestamp;
+    }
+
+
+    public String getApiKey() {
+        return apiKey;
+    }
+
+    public String getChannelNo() {
+        return channelNo;
+    }
+
+    public String getInterfaceName() {
+        return interfaceName;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+}
